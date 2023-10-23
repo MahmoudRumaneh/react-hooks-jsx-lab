75d6b4ed1a810f5a79e97da58242e383a95ad73c
MahmoudRumaneh <mahmoudrmn@gmail.com>
2023-10-23 11:05:48 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am a Computer Science student with a strong academic record and a passion for web development, seeking the web developer position at [Company Name]. With hands-on experience in front-end and back-end development, proficiency in languages like HTML5, CSS, PHP, JavaScript, and Java, and a demonstrated ability to transform ideas into functional web applications, I am well-equipped to contribute to your team. My problem-solving skills, commitment to user-friendly design, and background in data analytics and research further enhance my qualifications. Additionally, my volunteer work reflects my dedication to community involvement. I am eager to discuss how my skills align with your company's goals and contribute to your projects.</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
